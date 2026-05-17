797055e76aa3e38f0a66758842ac1d4b894aeb33
Ujjwal Sharma <ujjwalsharma3004@gmail.com>
2026-05-17 13:56:49 +0530

    fixed indentation properly

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = "8821928224:AAGXD025lJH5VaJlkSgAwC1Q25pLOBm9uwk"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-await update.message.reply_text("🔥 FloociBot is working!")
+    await update.message.reply_text("🔥 FloociBot is working!")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
